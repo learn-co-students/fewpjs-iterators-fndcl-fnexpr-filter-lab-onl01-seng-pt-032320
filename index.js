d4957f4996d1defe8f53750dfb0594e82a87ff4a
HMoreira2020 <moreiraheather@gmail.com>
2020-09-30 10:56:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(element => {return element.toLowerCase() === string.toLowerCase();});
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(element => {return element.startsWith(string);});
+}
+
+
+function matchName(array, string) {
+    return array.filter(element => {return element["name"].toLowerCase() === string.toLowerCase();}
+    );
+}
