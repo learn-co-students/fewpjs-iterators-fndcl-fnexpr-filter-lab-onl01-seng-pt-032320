1a689ca68af69d00939814889fcf4913ca4ac9f0
nabilhayet <nabil4457@yahoo.com>
2020-10-08 17:24:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(arr,str){
+    let exactMatch = arr.filter(n => {
+         return n.toLowerCase() === str.toLowerCase();
+    })
+    return exactMatch
+}
+
+function fuzzyMatch(arr, str){
+    let firstMatch = arr.filter(n => {
+
+        return n.slice(0,2) === str 
+    })
+    return firstMatch
+}
+
+
+function matchName(arr, str){
+    let nameMatch = arr.filter(n => {
+        return n.name === str 
+    })
+    return nameMatch
+}
