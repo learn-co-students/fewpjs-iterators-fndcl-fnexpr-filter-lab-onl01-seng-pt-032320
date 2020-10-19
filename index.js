3fd9c6c82dfebf6ca365d63f9f97183f39541db7
pledgedavis <pledgedavis14@gmail.com>
2020-10-19 00:55:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+
+
+
+
+// function superbowlWin(array){
+//     let result = array.find(obj => obj.result === "W")
+//     // record.find( function(s) { return s === "W" })
+//     return result ? result.year : undefined
+  
+    
+//   }
+  
+
+function findMatching(drivers, string) {
+    return drivers.filter( driver => driver.toLowerCase() === string.toLowerCase() )
+
+};
+    
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter( driver => driver.toLowerCase().indexOf(string.toLowerCase()) === 0  )
+
+};
+
+function matchName(drivers, string) {
+    return drivers.filter( driver => driver.name === string )
+}
